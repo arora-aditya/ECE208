1b9200a57499562c7947afe6a1ce1b2c4a21e63a
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2019-06-24 21:30:57 -0400

    saving NNF progress before cleanup/refactor into script - also update UTIL to generate as many NNFs as needed - added 10 more NNFs for testing

--- Util/generateNNF.py
+++ Util/generateNNF.py
@@ -1,23 +1,27 @@
-from random import * 
-
-
-	
+from random import *
+import sys
 #ext = randint(0,1111) # if you want this to be safe use something else
 ext = ""
-file_name = "test_case" + str(ext) + ".nnf" 
 
-if __name__ == "__main__": 
-	fp = open(file_name, "w+")
-	clauseLength = randint(5,10)
-	while (clauseLength > 0): 
-		var = randint(-10,10)
-		while (var == 0) :
+if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		count = int(sys.argv[1])
+	else:
+		count = 1
+	while(count):
+		file_name = "test_case" + str(count) + ".nnf"
+		fp = open(file_name, "w+")
+		clauseLength = randint(5,30)
+		while (clauseLength > 0):
 			var = randint(-10,10)
-		fp.write(str(var))
-		clauseLength = clauseLength-1
-		if (clauseLength > 0): 
-			operator = randint(0,1)
-			if (operator == 0) : 
-				fp.write(".")
-			else : 
-				fp.write("+")	
+			while (var == 0) :
+				var = randint(-10,10)
+			fp.write(str(var))
+			clauseLength = clauseLength-1
+			if (clauseLength > 0):
+				operator = randint(0,1)
+				if (operator == 0) :
+					fp.write(".")
+				else :
+					fp.write("+")
+		count -= 1
